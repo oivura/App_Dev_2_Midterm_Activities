14433c1ac76392288143958b311707ba4f722068
oivura <petersthanlie1026@gmail.com>
2024-04-29 02:16:56 +0800

    Activity_3_Controller

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProductController;
+
+Route::apiResource('products', ProductController::class);
 
 /*
 |--------------------------------------------------------------------------
